ec2bb4e25e30a660b7dcaaf81ee3923d27d9c107
aalves <alves.andre@netcabo.pt>
2014-11-04 22:50:58 +0100

    Added projects.html without routing.

--- js/app.js
+++ js/app.js
@@ -3,15 +3,21 @@
     var app = angular.module("app", []);
 
     var GitHubCtrl = function($scope, $http){
-        var onUserComplete = function(response){
-            $scope.user = response.data;
-        };
 
         var onError = function(reason){
-          $scope.error = "Error fetching the user";
+              $scope.error = "Error fetching projects information";
+          };
+
+        var onUserComplete = function(response){
+            $scope.user = response.data;
+            $http.get($scope.user.repos_url).then(onRepos, onError);
         };
 
-      $http.get("https://api.github.com/users/aalves").then(onUserComplete);
+        var onRepos = function(response){
+            $scope.repos = response.data;
+        };
+
+        $http.get("https://api.github.com/users/aalves").then(onUserComplete, onError);
     };
 
     app.controller("GitHubCtrl", ["$scope", "$http", GitHubCtrl]);
